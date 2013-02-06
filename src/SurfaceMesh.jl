8acba28b105e846b4561c384c6f8913bb4c34c9d
Michel Kuhlmann <michel@kuhlmanns.info>
2013-02-06 06:50:12 +0100

    implemented node-string parsing

--- src/SurfaceMesh.jl
+++ src/SurfaceMesh.jl
@@ -16,22 +16,36 @@ type Element
     mat :: Int
 end
 
+type NodeString
+    id :: String
+    nds :: Array{Int}
+end
 type Mesh
     id   :: String
     ele  :: Array{Element}
     nd   :: Array{Node}
-    ns   :: Array{Int}
+    ns   :: Array{NodeString}
 end
-
+# | Read a .2dm (SMS Aquaveo) mesh
 function read2dm(file)
-    parseNode(w) = Node(int(w[2]), float(w[3]), float(w[4]), float(w[5]))
-    parseTriangle(w) = Element(int(w[2]), int(w[2:4]), int(w[5]))
-    parseQuad(w) = Element(int(w[2]), int(w[2:5]), int(w[6]))
-    parseNs(w) = int(w[2:(length(w))])
+    parseNode(w::Array{String}) = Node(int(w[2]), float(w[3]), float(w[4]), float(w[5]))
+    parseTriangle(w::Array{String}) = Element(int(w[2]), int(w[2:4]), int(w[5]))
+    parseQuad(w::Array{String}) = Element(int(w[2]), int(w[2:5]), int(w[6]))
+    parseNsLine(l::String) = replace(l, r"^NS\s*", "")
+    # | Parse an array of nodes which are delimited by a zero-value and id
+    function parseNss(x::String)
+        x_split = map(split, split(x, " -"))
+        nss = Array(NodeString, 0)
+        for i = 1:(length(x_split)-1)
+            push!(nss, NodeString(x_split[i+1][1], int(x_split[i])))
+        end
+        nss
+        # ^ Returns an array of NodeString
+    end
     con = open(file, "r")
     nds = Array(Node,0)
     ele = Array(Element, 0)
-    nss = Array(Int, 0)
+    nsstr = ""
     for line = readlines(con)
         line = chomp(line)
         w = split(line)
@@ -42,12 +56,13 @@ function read2dm(file)
         elseif w[1] == "E4Q"
           push!(ele, parseQuad(w))
         elseif w[1] == "NS"
-          append!(nss, parseNs(w))
+          nsstr = join([nsstr, parseNsLine(line)], " ")
         else
           continue
         end
     end
     close(con)
+    nss = parseNss(nsstr)
     Mesh(replace(basename(file), ".2dm", ""), ele, nds, nss)
 end
 
