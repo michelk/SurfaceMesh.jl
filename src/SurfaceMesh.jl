2f3964a10b90c02a2d53b19ce8f43234eeb6814d
Michel Kuhlmann <michel@kuhlmanns.info>
2013-02-14 07:38:51 +0100

    using Dict for node- and element-map

--- src/SurfaceMesh.jl
+++ src/SurfaceMesh.jl
@@ -3,68 +3,81 @@ using Base
 export
   read2dm
  
-type Node
-    n :: Int
+# Index Element Mesh Represenation
+#=================================
+type Vertex
     x :: FloatingPoint
     y :: FloatingPoint
     z :: FloatingPoint
 end
 
-type Element
-    n   :: Int
-    ndi :: Array{Int}
-    mat :: Int
+type IndexElement
+    nds :: Array{Int}
+    mat :: Array{Int}
 end
 
-type NodeString
-    id :: String
-    nds :: Array{Int}
-end
-type Mesh
+type IndexElementMesh
     id   :: String
-    ele  :: Array{Element}
-    nd   :: Array{Node}
-    ns   :: Array{NodeString}
+    ele  :: Dict{Int, IndexElement}
+    nd   :: Dict{Int,Vertex}
+    ns   :: Dict{Int,Array{Int}}
+    mat  :: Dict{Int, String}
+end
+
+# Element Mesh Representation
+#============================
+
+type Element
+    n   :: Int
+    nds :: Array{Vertex}
+    mat :: Array{String}
 end
 # | Read a .2dm (SMS Aquaveo) mesh
 function read2dm(file)
-    parseNode(w::Array{String}) = Node(int(w[2]), float(w[3]), float(w[4]), float(w[5]))
-    parseTriangle(w::Array{String}) = Element(int(w[2]), int(w[2:4]), int(w[5]))
-    parseQuad(w::Array{String}) = Element(int(w[2]), int(w[2:5]), int(w[6]))
+    #file = "../test/ex.2dm"
+    parseNode(w::Array{String}) = (int(w[2]), float(w[3]), float(w[4]), float(w[5]))
+    parseTriangle(w::Array{String}) = (int(w[2]), int(w[2:4]), [int(w[5])])
+    parseQuad(w::Array{String}) = (int(w[2]), int(w[2:5]), [int(w[6])])
     parseNsLine(l::String) = replace(l, r"^NS\s*", "")
     # | Parse an array of nodes which are delimited by a zero-value and id
     function parseNss(x::String)
         x_split = map(split, split(x, " -"))
-        nss = Array(NodeString, 0)
+        nss = Dict{Int,Array{Int}}()
         for i = 1:(length(x_split)-1)
             nds = [int(x_split[i]), int(shift!(x_split[i+1]))]
-            push!(nss, NodeString(shift!(x_split[i+1]), nds))
+            nss[int(shift!(x_split[i+1]))] = nds
         end
         nss
         # ^ Returns an array of NodeString
     end
     con = open(file, "r")
-    nds = Array(Node,0)
-    ele = Array(Element, 0)
+    nd = Dict{Int,Vertex}() 
+    ele = Dict{Int, IndexElement}()
     nsstr = ""
+    mat = Dict{Int, String}()
+    #line = readline(con)
     for line = readlines(con)
         line = chomp(line)
         w = split(line)
         if w[1] == "ND"
-          push!(nds, parseNode(w))
+            (i,x,y,z) = parseNode(w)
+            nd[i] = Vertex(x,y,z)
         elseif w[1] == "E3T"
-          push!(ele, parseTriangle(w))
+            (i,n,m) = parseTriangle(w)
+            ele[i] = IndexElement(n,m)
         elseif w[1] == "E4Q"
-          push!(ele, parseQuad(w))
+            (i,n,m) = parseQuad(w)
+            ele[i] = IndexElement(n,m)
         elseif w[1] == "NS"
-          nsstr = join([nsstr, parseNsLine(line)], " ")
+            nsstr = join([nsstr, parseNsLine(line)], " ")
+        elseif w[1] == "MAT"
+            mat[int(w[2])] = w[3]
         else
-          continue
+            continue
         end
     end
     close(con)
     nss = parseNss(nsstr)
-    Mesh(replace(basename(file), ".2dm", ""), ele, nds, nss)
+    IndexElementMesh(replace(basename(file), ".2dm", ""), ele, nd, nss, mat)
 end
-
 end                                     # module SurfaceMesh
